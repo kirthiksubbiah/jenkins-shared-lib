084f7bce5bc22ae68d92739e1e39c8a5204cec3b
kirthiksubbiah <67821687+kirthiksubbiah@users.noreply.github.com>
2025-04-09 17:38:21 +0530

    Update commonBuild.groovy

--- vars/commonBuild.groovy
+++ vars/commonBuild.groovy
@@ -5,12 +5,12 @@ def call() {
 
     stage('Unit Tests') {
         echo 'Running unit tests...'
-        //sh './gradlew test'
+        
     }
 
     stage('Deploy') {
         echo 'Deploying to environment...'
-        //sh './deploy.sh dev'
+        
     }
 
 
